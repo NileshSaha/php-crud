a711d0039c20c6def266c36d246279936bb8ed03
NileshSaha <03.nileshsaha@gmail.com>
2019-09-27 22:30:20 +0530

    functions updated

--- core/functions.php
+++ core/functions.php
@@ -1,13 +1,6 @@
 <?php
     require 'connection.php';
 
-    // function addData(array $data){
-    //     $connect = db();
-    //     $fields = '"'.implode('","',$data).'"';
-    //     $sql = "INSERT INTO `students` (`name`,`class`,`section`) VALUES ($fields)";
-    //     $query = mysqli_query($connect,$sql) or die(mysqli_error($connect));
-    // }
-
     function view(){
         $connect = db();
         $sql = "SELECT * FROM `students`";
@@ -58,75 +51,4 @@
         $sql = "DELETE FROM `students` WHERE `id`='{$id}'";
         $query = mysqli_query($connect,$sql) or die(mysqli_error($connect));
     }
-/*
-    // function update($id){
-    //     $connect = db();
-    //     $sql = "SELECT `name`,`class`,`section` FROM `students` WHERE `id`='{$id}'";
-    //     $query = mysqli_query($connect,$sql);
-    //     $row = mysqli_fetch_assoc($query);
-    //     edit($row,$id);
-    // }
-
-    function edit($data,$id){
-        $fields = [
-            'name' ,
-            'class',
-            'section'
-        ];
-        $fieldvalue = [];
-        $errors = [];
-        if(isset($_POST['submit'])){
-            // foreach($fields as $field){
-            //     if(empty($_POST[$field])){
-            //         $errors[] = $field." value is missing.";
-            //     }
-            // }
-            if(empty($errors)){
-                foreach($fields as $field){
-                    $fieldvalue[$field]=$_POST[$field];
-                }
-                updateInDB($fieldvalue,$id);
-                //update value to database
-                echo "ok";
-                ?>
-                <script>
-                    window.location.href = '?action=view';
-                </script>
-                <?php
-            }else{
-               output_errors($errors);
-            }
-        }
-        ?>
-        <form action='' method="POST">
-            <ul>
-                <li>
-                    Name*:<br>
-                    <input type="text" name="name" value="<?php echo $data['name']?>";>
-                </li>
-                <li>
-                    Class*:<br>
-                    <input type="text" name="class" value="<?php echo $data['class']?>";>
-                </li>
-                <li>
-                    Section*:<br>
-                    <input type="text" name="section" value="<?php echo $data['section']?>";>
-                </li>
-            </ul>
-            <input type="submit" name="submit" value="submit">
-        </form>
-        <?php
-    }
-    // function updateInDB(array $data,$id){
-    //     $connect = db();
-    //     $sql = "UPDATE `students` SET `name`='{$data['name']}',`section`='{$data['section']}',`class`='{$data['class']}' WHERE `id`='{$id}'";
-    //     $query = mysqli_query($connect,$sql) or die(mysqli_error($connect));
-    // }
-
-    function output_errors($errors){
-        foreach($errors as $error){
-            echo $error.'<br>';
-        }
-    }
-    */
 ?>
